9e50024352b559c8bb17cfa473c692b02e090693
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-01-19 16:33:07 +0800

    add node manager

---
+++ src/node.ts
@@ -0,0 +1,65 @@
+import { ethers } from "ethers";
+import { Node } from "./types";
+
+export class NodeManager {
+	private readonly _provider: ethers.Provider;
+	private readonly _addr: string;
+	private readonly _abi: any[];
+
+	constructor(opt: {
+		provider: ethers.Provider
+		addr: string,
+		abi: any[]
+	}) {
+		this._provider = opt.provider;
+		this._addr = opt.addr;
+		this._abi = opt.abi;
+	}
+
+	public async getNodeInfo(id: string): Promise<Node | null | Error> {
+		try {
+			const contract = new ethers.Contract(this._addr, this._abi, this._provider);
+			const values: any[] = await contract.getNodeInfo(id);
+			const node = this._marshalNode(values);
+			return BigInt(node.pk) == 0n ? null : node;
+		} catch (err: any) {
+			return new Error(err);
+		}
+	}
+
+	public async addOrUpdate(backend: ethers.Signer, node: Node): Promise<Error | null> {
+		try {
+			const contract = new ethers.Contract(this._addr, this._abi, backend);
+			await contract.addOrUpdate(node);
+		} catch (err: any) {
+			return new Error(err);
+		}
+
+		return null;
+	}
+
+	public async remove(backend: ethers.Signer, id: string): Promise<Error | null> {
+		try {
+			const contract = new ethers.Contract(this._addr, this._abi, backend);
+			const node = this._marshalNode(await contract.getNodeInfo(id));
+			if (BigInt(node.pk) == 0n) {
+				return new Error("Node does not exist");
+			}
+			await contract.remove(id);
+		} catch (err: any) {
+			return new Error(err);
+		}
+
+		return null;
+	}
+
+	private _marshalNode(values: any[]): Node {
+		return {
+			pk: values[0],
+			owner: values[1],
+			teeType: values[2],
+			teeVer: values[3],
+			attestation: values[4]
+		};
+	}
+}
---
+++ test/node.test.ts
@@ -0,0 +1,69 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { ethers, network, artifacts } from "hardhat";
+import { expect } from "chai";
+import { Node } from "../src/types";
+import { NodeManager } from "../src/node";
+
+describe("NodeManager", function () {
+	async function deployFixture() {
+		const [backend] = await ethers.getSigners();
+
+		// deploy contracts
+		const NodeInfo = await ethers.getContractFactory("NodeInfo");
+		const nodeInfo = await NodeInfo.deploy(backend.address);
+
+		const randBytes = (numBytes: number) => {
+			return ethers.hexlify(ethers.randomBytes(numBytes));
+		}
+
+		const genNode = () => {
+			return {
+				pk: randBytes(32),
+				owner: randBytes(20),
+				teeType: randBytes(8),
+				teeVer: randBytes(8),
+				attestation: randBytes(32)
+			}
+		};
+		const nodes: Node[] = [genNode(), genNode(), genNode()];
+
+		const provider = new ethers.BrowserProvider(network.provider);
+		const nodeManager = new NodeManager({
+			provider: provider,
+			addr: await nodeInfo.getAddress(),
+			abi: (await artifacts.readArtifact("NodeInfo")).abi
+		});
+		return { nodeManager, nodeInfo, backend, nodes, randBytes };
+	}
+	describe("getNodeInfo", function () {
+		it("should return null if node does not exist", async function () {
+			const { randBytes, nodeManager } = await loadFixture(deployFixture);
+			expect(await nodeManager.getNodeInfo(randBytes(32))).to.be.null;
+		});
+		it("should return correct node info", async function () {
+			const { backend, nodeInfo, nodes, nodeManager } = await loadFixture(deployFixture);
+
+			await nodeInfo.connect(backend).addOrUpdate(nodes[0]);
+
+			expect(await nodeManager.getNodeInfo(nodes[0].pk)).to.deep.equal(nodes[0]);
+		});
+	});
+	describe("remove", function () {
+		it("Should return error when node does not exist", async function () {
+			const { randBytes, nodeManager, backend } = await loadFixture(deployFixture);
+			expect((await nodeManager.remove(backend, randBytes(32)))!.message).to.equal("Node does not exist");
+		});
+		it("Should return true when node exists", async function () {
+			const { nodeManager, backend, nodeInfo, nodes } = await loadFixture(deployFixture);
+			await nodeInfo.connect(backend).addOrUpdate(nodes[0]);
+			expect(await nodeManager.remove(backend, nodes[0].pk)).to.be.null;
+		});
+	});
+	describe("addOrUpdate", function () {
+		it("Should add correct node info", async function () {
+			const { nodeManager, backend, nodes } = await loadFixture(deployFixture);
+			expect(await nodeManager.addOrUpdate(backend, nodes[0])).to.be.null;
+			expect(await nodeManager.getNodeInfo(nodes[0].pk)).to.deep.equal(nodes[0]);
+		});
+	});
+});
