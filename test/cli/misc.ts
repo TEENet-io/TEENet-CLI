6ae4c8c3c33bcb327e4f68ff12e2ae8722f6c803
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-01-27 16:51:09 +0800

    update test

--- test/cli/misc.ts
+++ test/cli/misc.ts
@@ -1,23 +1,22 @@
 import { expect } from 'chai';
 import { test } from './common';
 import { randBytes } from '../../src/libs/common';
+import { WalletErr } from '../../src/cli/common';
 
 describe('CLI Misc', function () {
 	it('should detect invalid wallet index', function () {
-		const actual = test('code addOrUpdate 10 code0.json');
-		const expected = 'Invalid wallet index';
-		expect(actual).to.include(expected);
+		expect(test('code addOrUpdate 10 code0.json')).to.include(WalletErr.InvalidIndex(10).message);
 	});
 	it('should detect invalid wallet address', function () {
 		const addr = randBytes(21);
 		const actual = test(`code addOrUpdate ${addr} code0.json`);
-		const expected = 'Invalid wallet address';
+		const expected = WalletErr.InvalidAddress(addr).message;
 		expect(actual).to.include(expected);
 	});
 	it('should not find wallet', function () {
 		const addr = randBytes(20);
 		const actual = test(`code addOrUpdate ${addr} code0.json`);
-		const expected = 'Wallet not found';
+		const expected = WalletErr.NotFound(addr).message;
 		expect(actual).to.include(expected);
 	});
 });
