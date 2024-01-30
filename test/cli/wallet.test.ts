97f249a16bbb789493d42fbed55fda7d27dcafaa
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-01-30 14:25:49 +0800

    fix

--- test/cli/wallet.test.ts
+++ test/cli/wallet.test.ts
@@ -1,13 +1,22 @@
 import { expect } from 'chai';
-import { wallets, test } from './common';
+import { wallets } from './common';
 import { printAddresses } from '../../src/cli/common';
+import { execSync } from 'child_process';
+
+export function test(cmd: string): string {
+	try {
+		return execSync('npx ts-node ../../src/cli/cli.ts ' + cmd).toString();
+	} catch (err: any) {
+		return err.output[1].toString();
+	}
+}
 
 describe('CLI Wallet', function () {
 	describe('list', function () {
 		it('should list all wallets', function () {
 			const actual = test('wallet list');
 			const expected = printAddresses(Object.keys(wallets)) + '\n';
-			expect(actual).to.deep.equal(expected);
+			expect(actual).to.include(expected);
 		});
 	});
 });
